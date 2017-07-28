fb67fccff6bea6bc5a51d96755d99cb5ec49f311
Daniel Ng <dvng13@gmail.com>
2017-07-28 01:14:35 -0700

    update boot file

--- boot.rb
+++ boot.rb
@@ -14,8 +14,11 @@ require_relative "app/models/user_interface"
 # Require all the controllers
 require_relative "app/controllers/application_controller"
 require_relative "app/controllers/home_page_controller"
-require_relative "app/controllers/resource_search_controller"
+require_relative "app/controllers/search_fields_controller"
+require_relative "app/controllers/search_value_controller"
+require_relative "app/controllers/search_results_controller"
 
+# Initialize user interface
 user_interface = UserInterface.new
 home_page_controller = HomePageController.new
 user_interface.next(home_page_controller)
