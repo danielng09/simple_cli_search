b18ed6bcb471143faa37248ac85ba76a8709a316
Daniel Ng <dvng13@gmail.com>
2017-07-28 21:42:13 -0700

    init home page controller specs

---
+++ spec/controllers/home_page_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe HomePageController do
+
+  let(:home_page_controller) { HomePageController.new }
+  let(:user_interface) { UserInterface.new }
+
+  describe "constants" do
+    it "has a constant OPTION_TO_RESOURCE" do
+      constant = HomePageController::OPTION_TO_RESOURCE
+      expect(constant).to be_kind_of(Hash)
+      expect(constant.length).to be 3
+    end
+  end
+
+  describe "#post_handle_option" do
+    context "given valid option" do
+      it "selects the next controller" do
+        expect(user_interface).to receive(:next).with(kind_of(SearchFieldsController))
+        home_page_controller.post_handle_option(user_interface, '1')
+      end
+    end
+
+    context "given invalid option" do
+      it "handles the error" do
+        expect(home_page_controller).to receive(:handle_error)
+        home_page_controller.post_handle_option(user_interface, '4')
+      end
+    end
+  end
+
+end
