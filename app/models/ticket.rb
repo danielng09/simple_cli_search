9f8de1899c65af70099f661508fdbf1e8c94d99b
Daniel Ng <dvng13@gmail.com>
2017-07-28 19:28:37 -0700

    fix ticket and user relations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,7 @@
 class Ticket < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :submitter, class_name: "User", foreign_key: "submitter_id", primary_key: "_id"
+  belongs_to :assignee, class_name: "User", foreign_key: "assignee_id", primary_key: "_id"
+
   belongs_to :organization
 
   PRIMARY_ATTRIBUTES = ["_id", "external_id", "subject", "priority"].freeze
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   belongs_to :organization
-  has_many :tickets
+  has_many :submitted_tickets, class_name: "Ticket", foreign_key: "submitter_id", primary_key: "_id"
+  has_many :assigned_tickets, class_name: "Ticket", foreign_key: "assignee_id", primary_key: "_id"
 
   PRIMARY_ATTRIBUTES = ["_id", "external_id", "name", "email"].freeze
 
