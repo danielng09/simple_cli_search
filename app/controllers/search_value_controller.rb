34acca22cd9e3344d149146a394ad612e7aa96e4
Daniel Ng <dvng13@gmail.com>
2017-07-29 18:37:45 -0700

    move render methods to private in search value controller

--- app/controllers/search_value_controller.rb
+++ app/controllers/search_value_controller.rb
@@ -8,14 +8,6 @@ class SearchValueController < ApplicationController
 
   end
 
-  def render_body
-    header title: "Searching for '#{search_field}' field on #{resource_class.name.pluralize.downcase}", align: "center", bold: true
-  end
-
-  def render_options
-    aligned " * Type the '#{search_field}' value you'd like to search for"
-  end
-
   #
   # @param {UserInterface} user_interface
   # @param {String} option
@@ -31,4 +23,15 @@ class SearchValueController < ApplicationController
       handle_invalid_option(user_interface)
     end
   end
+
+  private
+
+  def render_body
+    header title: "Searching for '#{search_field}' field on #{resource_class.name.pluralize.downcase}", align: "center", bold: true
+  end
+
+  def render_options
+    aligned " * Type the '#{search_field}' value you'd like to search for"
+  end
+
 end
