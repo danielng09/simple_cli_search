fcac27d79e1c06e30087f1da5c7bf360a2cd7806
Daniel Ng <dvng13@gmail.com>
2017-07-29 22:11:58 -0700

    move render methods to private in detailed results controller

--- app/controllers/detailed_result_controller.rb
+++ app/controllers/detailed_result_controller.rb
@@ -8,6 +8,31 @@ class DetailedResultController < ApplicationController
     @search_result = resource_class.find_by(_id: search_value)
   end
 
+  #
+  # @param {UserInterface} user_interface
+  # @param {String} option
+  #
+  def post_handle_option(user_interface, option)
+    if @resource_class.reflections.keys.include?(option)
+      relation = @search_result.try(option)
+      results = if relation.is_a?(ActiveRecord::Relation)
+                  relation
+                else
+                  relation.class.where(_id: relation._id)
+                end
+      resource_class = results.first.class
+
+      controller = SearchResultsController.new(resource_class: resource_class,
+                                               results: results)
+      user_interface.next(controller)
+
+    else
+      handle_invalid_option(user_interface)
+    end
+  end
+
+  private
+
   def render_body
     header title: "Showing detailed result for #{resource_class.name.downcase}", align: "center", bold: true
     vertical_spacing 1
@@ -36,29 +61,6 @@ class DetailedResultController < ApplicationController
   def render_options
     @resource_class.reflections.keys.each do |relation|
       aligned " * Type '#{relation}' if you'd like to view the corresponding #{relation}"
-    end
-  end
-
-  #
-  # @param {UserInterface} user_interface
-  # @param {String} option
-  #
-  def post_handle_option(user_interface, option)
-    if @resource_class.reflections.keys.include?(option)
-      relation = @search_result.try(option)
-      results = if relation.is_a?(ActiveRecord::Relation)
-                  relation
-                else
-                  relation.class.where(_id: relation._id)
-                end
-      resource_class = results.first.class
-
-      controller = SearchResultsController.new(resource_class: resource_class,
-                                               results: results)
-      user_interface.next(controller)
-
-    else
-      handle_invalid_option(user_interface)
     end
   end
 end
